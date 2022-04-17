83710284b88c6d6cfe0f5615d746e255e13fe366
arnobtarafder <rmtarnob@gmail.com>
2022-04-18 00:28:38 +0600

    courses description & images changes

--- src/components/Home/Course/Course.js
+++ src/components/Home/Course/Course.js
@@ -14,14 +14,16 @@ const Course = ({ courses }) => {
             <div className='p-3'>
                 <h2>{name}</h2>
                 <p>Price: {price}</p>
-                <p className='pb-5 fs-5'><small>{
+                <p className='fs-5'><small>{
                     description.length < 378
                         ? description
                         :
                         <div>
-                            {description.slice(0, 278)}<span className=''><button onClick={() => navigateToServiceDetail(id)} className='btn btn-link text-decoration-none ps-0'>...read more</button></span> 
+                            {description.slice(0, 178)}<span className=''><button onClick={() => navigateToServiceDetail(id)} className='btn btn-link text-decoration-none ps-0'>...read more</button></span> 
                         </div>
                     }</small></p>
+
+                    <h5 className='pb-5 fw-light text-primary'>Instructor: Mr.Arnob Tarafder</h5>
                 <Link to="/checkout" className='btn btn-primary position-absolute top-auto bottom-0'>Book Now</Link>
             </div>
         </div>
--- src/components/Home/Courses/Courses.js
+++ src/components/Home/Courses/Courses.js
@@ -12,7 +12,7 @@ const Courses = () => {
     }, [])
 
     return (
-        <div id='services' className='container'>
+        <div id='Courses' className='container'>
 
             <div className="row">
             <h1 className='text-primary text-center mt-5'>My Courses: {courses.length}</h1>
