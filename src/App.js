28460d0fa2143d81e6c640d8cae3f4c353353490
arnobtarafder <rmtarnob@gmail.com>
2022-04-17 16:02:41 +0600

    header collapse icon color solved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Authentication/Login/Login';
+import Registration from './components/Authentication/Registration/Registration';
 import Header from './components/Shared/Header/Header';
 
 function App() {
@@ -7,8 +9,8 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/login'>Login</Route>
-        <Route path='/registration'>Registration</Route>
+        <Route path='/login' element={<Login />}>Login</Route>
+        <Route path='/registration' element={<Registration />}>Registration</Route>
       </Routes>
     </div>
   );
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -15,65 +15,105 @@ const Header = () => {
 
 
   return (
-    <nav className='navbar navbar-expand navbar-light bg-white py-2  sticky-top'>
-      <div className="container">
-          <Navbar.Brand as={Link} to="/">
-            <img height={30} src="https://cdn-icons-png.flaticon.com/128/987/987748.png?ga=GA1.1.208812877.1649674070" type="image/x-icon" alt="" /> <span className='ps-1 pe-4'>EduHash-Mentor</span>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/home#services">Blogs</Nav.Link>
-              <Nav.Link href="/home#experts">Services</Nav.Link>
-              <NavDropdown title="Courses" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="https://github.com/robert" target="_blank">Bangla</NavDropdown.Item>
-                <NavDropdown.Item href="https://github.com/john" target="_blank">Math</NavDropdown.Item>
-                <NavDropdown.Item href="https://github.com/kiron0" target="_blank">English</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4"></NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            </Navbar.Collapse>
+    <Navbar sticky="top" expand="lg" bg="light">
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/">
+          <img height={30} src="https://cdn-icons-png.flaticon.com/128/987/987748.png?ga=GA1.1.208812877.1649674070" type="image/x-icon" alt="" /> <span className='ps-1 pe-4 text-secondary'>EduHash-Mentor</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className='text-dark' href="/home#experts">Blogs</Nav.Link>
+            <Nav.Link  className='text-dark' href="/home#services">Services</Nav.Link>
 
-        <ul className='navbar-nav align-items-center'>
+            <NavDropdown title="Courses" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="https://github.com/robert" target="_blank">Bangla</NavDropdown.Item>
+              <NavDropdown.Item href="https://github.com/john" target="_blank">Math</NavDropdown.Item>
+              <NavDropdown.Item href="https://github.com/kiron0" target="_blank">English</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4"></NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
 
-          <li className='nav-item'>
-            {
-              auth.user ?
-                <Link to='/checkout' className='nav-link'>
-                  {auth.user.displayName}
-                  <img className='ml-3' src={auth.user.photoURL ? auth.user.photoURL : "https://cdn-icons-png.flaticon.com/128/1177/1177568.png"} width="35px" alt="" />
-                </Link>
-                :
-                <Link to='/login' className='nav-link'>Login</Link>
-            }
-          </li>
+            <Nav.Item>
+              {
+                auth.user ?
+                  <Link to='/checkout' className='nav-link'>
+                    {auth.user.displayName}
+                    <img className='ml-3' src={auth.user.photoURL ? auth.user.photoURL : "https://cdn-icons-png.flaticon.com/128/1177/1177568.png"} width="35px" alt="" />
+                  </Link>
+                  :
+                  <Link to='/login' className='nav-link pt-3 pe-4 fw-bolder'>Login</Link>
+              }
+            </Nav.Item>
 
-          <li className='nav-item'>
-            {
-              auth.user ?
-                <Link to='/' className='nav-link'>
-                  <button onClick={() => { auth.signOut() }}
-                    className='btn btn-danger btn-rounded'
-                  >
-                    Log Out
-                  </button>
-                </Link>
-                :
-                <Link to='/registration' className='nav-link'>
-                  <button
-                    className='btn btn-danger btn-rounded'
-                  >
-                    Registration
-                  </button>
-                </Link>
-            }
-          </li>
-        </ul>
-      </div>
-    </nav>
+            <Nav.Item>
+
+              {
+                auth.user ?
+                  <Link to='/' className='nav-link'>
+                    <button onClick={() => { auth.signOut() }}
+                      className='btn btn-danger btn-rounded'
+                    >
+                      Log Out
+                    </button>
+                  </Link>
+                  :
+                  <Link to='/registration' className='nav-link'>
+                    <button
+                      className='btn btn-danger btn-rounded'
+                    >
+                      Registration
+                    </button>
+                  </Link>
+              }
+
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
 export default Header;
+
+
+// className='navbar-nav align-items-center'
+<Nav>
+
+  <Nav.Item>
+    {
+      auth.user ?
+        <Link to='/checkout' className='nav-link'>
+          {auth.user.displayName}
+          <img className='ml-3' src={auth.user.photoURL ? auth.user.photoURL : "https://cdn-icons-png.flaticon.com/128/1177/1177568.png"} width="35px" alt="" />
+        </Link>
+        :
+        <Link to='/login' className='nav-link'>Login</Link>
+    }
+  </Nav.Item>
+
+  <Nav.Item>
+    {
+      auth.user ?
+        <Link to='/' className='nav-link'>
+          <button onClick={() => { auth.signOut() }}
+            className='btn btn-danger btn-rounded'
+          >
+            Log Out
+          </button>
+        </Link>
+        :
+        <Link to='/registration' className='nav-link'>
+          <button
+            className='btn btn-danger btn-rounded'
+          >
+            Registration
+          </button>
+        </Link>
+    }
+  </Nav.Item>
+</Nav>
